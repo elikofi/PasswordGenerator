99d6aa051f2d92aed156e745be16833e4c128471
elijah amoako <elijahamoako72@gmail.com>
2022-10-09 06:14:52 +0000

    Updated to the best version

---
+++ PasswordGenerator/Generator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace PasswordGenerator
+{
+    public class Generator
+    {
+
+        public static string GeneratePassword(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "1234567890" +
+                "@$!%&*+_";
+
+            StringBuilder builder = new StringBuilder();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = RandomNumberGenerator.GetInt32(chars.Length);
+
+                builder.Append(chars[index]);
+            }
+            return builder.ToString();
+        }
+        public Generator()
+        {
+        }
+    }
+}
+
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -1,55 +1,37 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using PasswordGenerator;
+using System.Linq;
+using System.Collections.Generic;
 
-do
+try
 {
-    try
+    do
     {
         Console.WriteLine("Enter the length you want the password to be: ");
         int length = Convert.ToInt32(Console.ReadLine());
 
 
-        if (length > 0)
+        if (length >= 7)
         {
-            Console.WriteLine($"Your password is: {GeneratePassword(length)}");
+            Console.WriteLine($"Your password is: {Generator.GeneratePassword(length)}");
         }
-        else
+        else if(length < 7)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Enter a valid input.");
+            Console.WriteLine("Password length is too short. In order to get a stronger" +
+                " password, length has to be higher than seven.");
             Console.ResetColor();
         }
         Console.ReadLine();
-
-
-        //method
-
-        static string GeneratePassword(int length)
-        {
-            const string chars = "abcdefghijklmnopqrstuvwxyz" +
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                "1234567890" +
-                "@$!%&*+_";
-
-            StringBuilder builder = new StringBuilder();
-
-            Random rnd = new Random();
-
-            for (int i = 0; i < length; i++)
-            {
-                int index = RandomNumberGenerator.GetInt32(chars.Length);
-
-                builder.Append(chars[index]);
-            }
-            return builder.ToString();
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Enter a valid input (number).");
-        Console.ReadLine();
-        Console.ResetColor();
-    }
-    Console.WriteLine("Do you want to generate a new password? ( Yes = Y / No = N ).");
-} while (Console.ReadLine().ToUpper() == "Y");
+        Console.WriteLine("Do you want to generate a new password? - ( Yes = Y / No = N ).");
+    } while (Console.ReadLine().ToUpper() == "Y");
+    
+}
+catch (FormatException)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Value entered was not in the correct format. Length must be an integer.");
+    Console.ReadLine();
+    Console.ResetColor();
+}
